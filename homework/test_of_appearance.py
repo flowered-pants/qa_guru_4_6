48224963fd24568733bb799155ca52f4a1aa6ffa
Evgenia Belikova <98419537+flowered-pants@users.noreply.github.com>
2023-03-22 15:01:55 +0530

    Update test_of_appearance.py
    fixes 3.0

--- homework/test_of_appearance.py
+++ homework/test_of_appearance.py
@@ -54,26 +54,27 @@ print(test_find_suitable_user.__name__)
     # >>> open_browser(browser_name="Chrome")
     # "Open Browser [Chrome]"
 
+def correction_name_of_readable_function(func, *args):
+    description_of_function = f'{func.__name__.title().replace("_", " ")} [{", ".join(args)}]'
+    print(description_of_function)
+    return description_of_function
 
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
-def correction_name_of_readable_function(func, **kwargs):
-    description_of_function = f'{func.__name__.title().replace("_", " ")} [{", ".join(kwargs)}]'
-    print(description_of_function)
-    return description_of_function
 
 def open_browser(browser_name):
-    actual_result = correction_name_of_readable_function(browser_name="Chrome")
+    actual_result = correction_name_of_readable_function(open_browser, browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
 
 def go_to_companyname_homepage(page_url):
-    actual_result = correction_name_of_readable_function(page_url= "https://companyname.com")
+    actual_result = correction_name_of_readable_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
+
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = correction_name_of_readable_function(page_url = "https://companyname.com/login", button_text = "Register")
+    actual_result = correction_name_of_readable_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
-
-
